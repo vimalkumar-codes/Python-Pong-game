71b3902c6d8a9e77e855bd51decc38b0f0bfe16e
Vimal Kumar <vimalkumar.codes@gmail.com>
2026-05-01 22:34:20 +0530

    Detect paddle when paddle goes out of bounds

--- main.py
+++ main.py
@@ -34,4 +34,12 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
+    #Detect R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    #Detect L paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+
 screen.exitonclick()
